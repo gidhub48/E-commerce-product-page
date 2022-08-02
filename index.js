cd385979aba49c340517d9be3a80d8f79e5984a4
sarah smith01 <tooktun2547@gmail.com>
2022-08-02 15:04:32 +0700

    third commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let openMenu = document.getElementById("open-menu")
 let closeMenu = document.getElementById("close-menu")
 
 let menu = document.getElementById("mobile-menu")
-let menuBackdrop = document.getElementById("mobile-menu-backdrop")
+let backdrop = document.getElementById("backdrop")
 
 let avatar = document.getElementById("avatar")
 let cart = document.getElementById("cart")
@@ -11,16 +11,16 @@ let clientOrders = document.getElementById("client-orders")
 
 closeMenu.addEventListener("click", function() {
     menu.style.transform = "translateX(-100%)"
-    menuBackdrop.style.opacity = "0"
+    backdrop.style.opacity = "0"
     setTimeout(() => {
-        menuBackdrop.style.display = "none"
+        backdrop.style.display = "none"
     }, 300);
 
 })
 
 openMenu.addEventListener("click", function() {
-    menuBackdrop.style.display = "block"
-    menuBackdrop.style.opacity = "0.75"
+    backdrop.style.display = "block"
+    backdrop.style.opacity = "0.75"
     menu.style.transform = "translateX(0)"
 })
 
@@ -118,6 +118,123 @@ checkout.addEventListener("click", function() {
     purchasingSound.play()
     cartN.innerHTML = ""
 })
+
+// preview code
+
+let mainPreview = document.querySelector("#mainPreview>img")
+let thumbnail = document.querySelectorAll(".thumbnail>img")
+let thumbnailPa = document.querySelectorAll(".thumbnail")
+let select = 0
+
+for (let i = 0; i < thumbnail.length; i++) {
+    thumbnail[i].addEventListener("click", () => {
+        let a = thumbnail[i]
+        let b = a.getAttribute("src")
+        let c = b.replace("-thumbnail", "")
+        mainPreview.setAttribute("src", c)
+        for (let i = 0; i < thumbnail.length; i++) {
+            thumbnailPa[i].classList.remove("select")
+        }
+        thumbnailPa[i].classList.add("select")
+
+    })
+}
+
+let next_btn = document.querySelector("#next-btn")
+let previous_btn = document.querySelector("#previous-btn")
+
+next_btn.addEventListener("click",()=>{
+    if (select !== thumbnail.length-1) {
+        select++
+        mainPreview.setAttribute("src", thumbnail[select].getAttribute("src").replace("-thumbnail", ""))
+    }
+    if (select === thumbnail.length-1) {
+        select=0
+        mainPreview.setAttribute("src", thumbnail[select].getAttribute("src").replace("-thumbnail", ""))
+    }
+})
+previous_btn.addEventListener("click",()=>{
+    if (select !== 0) {
+        select--
+        mainPreview.setAttribute("src", thumbnail[select].getAttribute("src").replace("-thumbnail", ""))
+    }
+    if (select === 0) {
+        select=thumbnail.length-1
+        mainPreview.setAttribute("src", thumbnail[select].getAttribute("src").replace("-thumbnail", ""))
+    }
+})
+
+let lightbox = document.querySelector("#lightbox")
+let close_lightbox = document.querySelector("#close-lightbox>svg")
+
+
+mainPreview.addEventListener("click", ()=>{
+    if (window.innerWidth > 520) {
+        lightbox.style.display = "block"
+        backdrop.style.display = "block"
+        backdrop.style.opacity = "0.75"
+    }
+})
+close_lightbox.addEventListener("click", ()=>{
+    lightbox.style.display = "none"
+    backdrop.style.display = "none"
+})
+
+
+let lb_thumbnail = document.querySelectorAll(".lightbox-thumbnail")
+let lb_mainImg = document.querySelector("#ligthbox-main-img>#main-image")
+let prev_selected = 0
+let selected = 0
+
+for (let i = 0; i < lb_thumbnail.length; i++) {
+    lb_thumbnail[i].addEventListener("click", ()=>{
+        console.log(lb_mainImg.src);
+        lb_mainImg.setAttribute("src", `images/image-product-${i+1}.jpg`);
+        lb_thumbnail[prev_selected].classList.remove("select")
+        lb_thumbnail[i].classList.add("select")
+        prev_selected = i
+        selected = i
+    })
+}
+
+let lb_next_btn = document.querySelector("#lb-next-btn")
+let lb_previous_btn = document.querySelector("#lb-previous-btn")
+
+function changeImg(prev_selected, selected) {
+    lb_thumbnail[prev_selected].classList.remove("select")
+    lb_thumbnail[selected].classList.add("select")
+    lb_mainImg.setAttribute("src", lb_thumbnail[selected].getAttribute("src").replace("-thumbnail", ""));
+}
+
+
+lb_next_btn.addEventListener("click", ()=>{
+
+    if (selected !== lb_thumbnail.length-1) {
+        selected++
+        changeImg(prev_selected,selected)
+        prev_selected++
+
+    }else if (prev_selected === lb_thumbnail.length-1) {
+        selected=0
+        changeImg(prev_selected,selected)
+        prev_selected=0
+    }
+
+})
+
+lb_previous_btn.addEventListener("click", ()=>{
+    if (selected !== 0) {
+        selected--
+        changeImg(prev_selected,selected)
+        prev_selected--
+
+    }else if (selected === 0) {
+        selected = lb_thumbnail.length-1
+        changeImg(prev_selected,selected)
+        prev_selected = lb_thumbnail.length-1
+    }
+})
+
 
 
 
